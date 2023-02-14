a9180762777765e7770801f74795e274b719a57e
bellamyphan <bellamyphan@icloud.com>
2023-02-14 16:54:25 -0600

    Layout the main menu view

--- src/main/java/application/MainController.java
+++ src/main/java/application/MainController.java
@@ -1,13 +1,32 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
-    public void onStartButtonClick() {
+    private Button onStartButtonClick;
+
+    @FXML
+    public void onStartButtonClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main-menu-view.fxml"));
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        Scene scene = new Scene(loader.load(), bounds.getWidth(), bounds.getHeight());
+        Stage stage = (Stage) onStartButtonClick.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 
     @FXML
     public void onExitButtonClick() {
+        System.exit(0);
     }
 }
---
+++ src/main/java/application/MainMenuController.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.fxml.FXML;
+
+public class MainMenuController {
+    @FXML
+    public void transactionMenuClick() {
+    }
+
+    @FXML
+    public void bankMenuClick() {
+    }
+
+    @FXML
+    public void reportMenuClick() {
+    }
+
+    @FXML
+    public void exportMenuClick() {
+    }
+
+    @FXML
+    public void investmentAndSavingMenuClick() {
+    }
+
+    @FXML
+    public void netWorthMenuClick() {
+    }
+
+    @FXML
+    public void exitMainMenuClick() {
+    }
+}
