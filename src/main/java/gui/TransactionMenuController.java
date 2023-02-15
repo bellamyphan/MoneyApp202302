4eae944fb736138b5823c61eaaa81175698330c5
Bellamy Phan <bellamyphan@icloud.com>
2023-02-15 15:05:03 -0600

    Add the exit button for transaction menu

--- src/main/java/gui/TransactionMenuController.java
+++ src/main/java/gui/TransactionMenuController.java
@@ -1,4 +1,25 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import tools.StageHandler;
+
+import java.io.IOException;
+
 public class TransactionMenuController {
+    @FXML
+    private Button onExitThisMenuButton;
+
+    @FXML
+    public void onExitThisMenuClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/main-menu-view.fxml"));
+        Scene scene = new Scene(loader.load());
+        Stage stage = (Stage) onExitThisMenuButton.getScene().getWindow();
+        stage.setScene(scene);
+        new StageHandler().setStageMaximized(stage);
+        stage.show();
+    }
 }
