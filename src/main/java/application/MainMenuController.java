720c11b10c54d346e1c151301c3d7fc2d60cf09a
bellamyphan <bellamyphan@icloud.com>
2023-02-14 18:46:30 -0600

    Implement exit main menu button

--- src/main/java/application/MainMenuController.java
+++ src/main/java/application/MainMenuController.java
@@ -1,33 +1,48 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import tools.PrimaryScreen;
+
+import java.io.IOException;
 
 public class MainMenuController {
     @FXML
-    public void transactionMenuClick() {
+    private Button onExitMainMenuButton;
+
+    @FXML
+    public void onTransactionMenuClick() {
     }
 
     @FXML
-    public void bankMenuClick() {
+    public void onBankMenuClick() {
     }
 
     @FXML
-    public void reportMenuClick() {
+    public void onReportMenuClick() {
     }
 
     @FXML
-    public void exportMenuClick() {
+    public void onExportMenuClick() {
     }
 
     @FXML
-    public void investmentAndSavingMenuClick() {
+    public void onInvestmentAndSavingMenuClick() {
     }
 
     @FXML
-    public void netWorthMenuClick() {
+    public void onNetWorthMenuClick() {
     }
 
     @FXML
-    public void exitMainMenuClick() {
+    public void onExitMainMenuClick() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("application-view.fxml"));
+        Scene scene = new Scene(loader.load(), PrimaryScreen.primaryScreenWidth, PrimaryScreen.primaryScreenHeight);
+        Stage stage = (Stage) onExitMainMenuButton.getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
     }
 }
