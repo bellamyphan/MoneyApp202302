b777fa1a4273873cd770b2450f76efe4396ff1ac
Bellamy Phan <bellamyphan@icloud.com>
2023-02-15 15:12:31 -0600

    Add bank menu

---
+++ src/main/java/gui/BankMenuController.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javafx.fxml.FXML;
+
+public class BankMenuController {
+    @FXML
+    public void onShowAllBanksClick() {
+    }
+
+    @FXML
+    public void onShowAllBankBalancesClick() {
+    }
+
+    @FXML
+    public void onAddABankClick() {
+    }
+
+    @FXML
+    public void onExitThisMenuClick() {
+    }
+}
