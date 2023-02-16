717df7d6e1145b94ddc22d7583952dbb3494527c
Bellamy Phan <bellamyphan@icloud.com>
2023-02-16 12:20:38 -0600

    Add a ComboBox for type in add-a-transaction-view.fxml

--- src/main/java/gui/transaction/AddATransactionController.java
+++ src/main/java/gui/transaction/AddATransactionController.java
@@ -1,4 +1,19 @@
 package gui.transaction;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import objects.Type;
+import objects.TypeHandler;
+
 public class AddATransactionController {
+    @FXML
+    private ComboBox<Type> typeComboBox;
+
+    @FXML
+    public void initialize() {
+        ObservableList<Type> typeObservableList = FXCollections.observableArrayList(new TypeHandler().getTypeList());
+        typeComboBox.setItems(typeObservableList);
+    }
 }
--- src/main/java/objects/TypeHandler.java
+++ src/main/java/objects/TypeHandler.java
@@ -1,16 +1,26 @@
 package objects;
-//
-//import java.util.Arrays;
-//import java.util.List;
-//
-//public class TypeHandler {
-//    private final List<Type> typeList;
-//
-//    public TypeHandler() {
-//        typeList = Arrays.asList(Type.values());
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TypeHandler {
+    private final List<Type> typeList;
+
+    public TypeHandler() {
+        typeList = Arrays.asList(Type.values());
+    }
+
+    public List<Type> getTypeList() {
+        return typeList;
+    }
+
+//    public List<Type> getTypeList(String searchText) {
+//        List<Type> resultType = new ArrayList<>();
+//        for (Type type : typeList) {
+//            if (type.toString().toLowerCase().contains(searchText.toLowerCase())) {
+//                resultType.add(type);
+//            }
+//        }
+//        return resultType;
 //    }
-//
-//    public List<Type> getTypeList() {
-//        return typeList;
-//    }
-//}
+}
