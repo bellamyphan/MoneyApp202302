e56355a491f85c29a229c0f513834b93349e1027
Bellamy Phan <bellamyphan@icloud.com>
2023-02-16 00:53:12 -0600

    Add id and type when creating a transaction

--- src/main/java/gui/transaction/AddATransactionController.java
+++ src/main/java/gui/transaction/AddATransactionController.java
@@ -1,4 +1,41 @@
 package gui.transaction;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import objects.Type;
+import objects.TypeHandler;
+
 public class AddATransactionController {
+    @FXML
+    private ComboBox<Type> typeComboBox;
+
+    @FXML
+    public void initialize() {
+        ObservableList<Type> observableList = FXCollections.observableArrayList();
+        observableList.addAll(new TypeHandler().getTypeList());
+        typeComboBox.setItems(observableList);
+        typeComboBox.setEditable(true);
+    }
+
+    @FXML
+    public void idOnMouseClicked(MouseEvent mouseEvent) {
+        Node node = (Node) mouseEvent.getSource();
+        TextField textField = (TextField) node;
+        textField.clear();
+        System.out.println("Mouse clicked");
+    }
+
+    @FXML
+    public void idOnKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().compareTo(KeyCode.ENTER) == 0) {
+            System.out.println("Enter key pressed");
+        }
+    }
 }
---
+++ src/main/java/objects/TypeHandler.java
@@ -0,0 +1,16 @@
+package objects;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TypeHandler {
+    private final List<Type> typeList;
+
+    public TypeHandler() {
+        typeList = Arrays.asList(Type.values());
+    }
+
+    public List<Type> getTypeList() {
+        return typeList;
+    }
+}
