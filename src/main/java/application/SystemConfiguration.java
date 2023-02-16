d74986fc050c36febe0722bc8edb2b34d8fbfb87
Bellamy Phan <bellamyphan@icloud.com>
2023-02-15 21:00:26 -0600

    Add a directory for menu views

--- src/main/java/application/SystemConfiguration.java
+++ src/main/java/application/SystemConfiguration.java
@@ -2,7 +2,7 @@ package application;
 
 public class SystemConfiguration {
     public static String applicationViewPath = "/application/application-view.fxml";
-    public static String bankMenuViewPath = "/gui/bank-menu-view.fxml";
-    public static String mainMenuViewPath = "/gui/main-menu-view.fxml";
-    public static String transactionMenuViewPath = "/gui/transaction-menu-view.fxml";
+    public static String bankMenuViewPath = "/gui/menu/bank-menu-view.fxml";
+    public static String mainMenuViewPath = "/gui/menu/main-menu-view.fxml";
+    public static String transactionMenuViewPath = "/gui/menu/transaction-menu-view.fxml";
 }
