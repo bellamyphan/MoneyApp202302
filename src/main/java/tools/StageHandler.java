09eab2055c859dcfa4402ebd4283a83ed2be8a11
Bellamy Phan <bellamyphan@icloud.com>
2023-02-15 21:42:25 -0600

    Enhance StageHandler.java

--- src/main/java/tools/StageHandler.java
+++ src/main/java/tools/StageHandler.java
@@ -1,7 +1,5 @@
 package tools;
 
-import application.MainApplication;
-import application.SystemConfiguration;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -13,16 +11,6 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class StageHandler {
-    public void startTheApplication(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(
-                MainApplication.class.getResource(SystemConfiguration.applicationViewPath));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle(SystemConfiguration.applicationTitle);
-        stage.setScene(scene);
-        setStageMaximized(stage);
-        stage.show();
-    }
-
     public void goToView(ActionEvent actionEvent, String viewPath) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource(viewPath));
         Scene scene = new Scene(loader.load());
@@ -33,7 +21,15 @@ public class StageHandler {
         stage.show();
     }
 
-    public void setStageMaximized(Stage stage) {
+    public void goToView(Stage stage, String viewPath) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource(viewPath));
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        setStageMaximized(stage);
+        stage.show();
+    }
+
+    private void setStageMaximized(Stage stage) {
         String osName = System.getProperty("os.name");
         if (osName.compareTo("Windows 10") == 0 || osName.compareTo("Windows 11") == 0) {
             Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
