e8c1e49351463c982425878d1c179d5d835831c2
Bellamy Phan <bellamyphan@icloud.com>
2023-02-17 03:31:30 -0600

    Create TransactionObject.java

---
+++ src/main/java/objects/transaction/TransactionObject.java
@@ -0,0 +1,21 @@
+package objects.transaction;
+
+//import objects.amount.AmountObject;
+//import objects.bank.BankObject;
+//import objects.location.LocationObject;
+//import objects.type.Type;
+//
+//import java.util.Date;
+//
+//public class TransactionObject {
+//    private Integer id;
+//    private Integer parent_id;
+//    private Type type;
+//    private Date date;
+//    private AmountObject amount;
+//    private String note;
+//    private String name;
+//    private LocationObject locationObject;
+//    private BankObject primaryBank, secondaryBank;
+//    private Boolean isPending;
+//}
