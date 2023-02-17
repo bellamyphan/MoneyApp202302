e2789ded68377bec14437d0b4d8f0b80047db5a8
Bellamy Phan <bellamyphan@icloud.com>
2023-02-16 22:41:52 -0600

    Create BankObject.java

--- src/main/java/objects/bank/BankObject.java
+++ src/main/java/objects/bank/BankObject.java
@@ -1,11 +1,52 @@
 package objects.bank;
 
-//public class BankObject {
-//    private String bankName;
-//    private String website;
-//    private String accountName;
-//    private Date openDate;
-//    private Date closeDate;
-//    private BankType accountType;
-//    private Double interestRate;
-//}
+import java.util.Date;
+
+public class BankObject {
+    private final String bankName;
+    private final String website;
+    private final String accountName;
+    private final Date openDate;
+    private final Date closeDate;
+    private final BankType accountType;
+    private final Double interestRate;
+
+    public BankObject(String bankName, String website, String accountName, Date openDate, Date closeDate,
+                      BankType accountType, Double interestRate) {
+        this.bankName = bankName;
+        this.website = website;
+        this.accountName = accountName;
+        this.openDate = openDate;
+        this.closeDate = closeDate;
+        this.accountType = accountType;
+        this.interestRate = interestRate;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public Date getOpenDate() {
+        return openDate;
+    }
+
+    public Date getCloseDate() {
+        return closeDate;
+    }
+
+    public BankType getAccountType() {
+        return accountType;
+    }
+
+    public Double getInterestRate() {
+        return interestRate;
+    }
+}
