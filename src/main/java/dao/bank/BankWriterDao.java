9ee57dcf5acd6de18b5e892ee1180fbf764c6285
Bellamy Phan <bellamyphan@icloud.com>
2023-02-16 21:51:21 -0600

    Create BankWriterDao.java

---
+++ src/main/java/dao/bank/BankWriterDao.java
@@ -0,0 +1,6 @@
+package dao.bank;
+
+//public class BankWriterDao {
+//    private final String[] headerLineElements = new String[]{"Bank Name", "Website", "Account Name", "Open Date",
+//            "Close Date", "Account Type", "Interest Rate"};
+//}
