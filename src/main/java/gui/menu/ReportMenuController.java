40f1108b05f0270c753e255182e3e3a3b329ea82
bellamyphan <bellamyphan@icloud.com>
2023-05-12 02:33:41 -0500

    Finished quick-button feature

--- src/main/java/gui/menu/ReportMenuController.java
+++ src/main/java/gui/menu/ReportMenuController.java
@@ -21,7 +21,8 @@ import java.util.List;
 
 public class ReportMenuController {
     @FXML
-    private Button inputYearButton, startMonthEndMonthButton, confirmButton, quickButton1, quickButton2, quickButton3;
+    private Button inputYearButton, startMonthEndMonthButton, confirmButton,
+            quickButton1, quickButton2, quickButton3;
     @FXML
     private Label startDateLabel;
     @FXML
@@ -66,10 +67,58 @@ public class ReportMenuController {
     }
 
     @FXML
+    private void quickButton1OnAction(ActionEvent actionEvent) throws IOException {
+        inputYearButton.setDisable(true);
+        startMonthEndMonthButton.setDisable(true);
+        reportType = ReportType.QUICK_BUTTON_1;
+        confirmButtonOnAction(actionEvent);
+    }
+
+    @FXML
+    private void quickButton2OnAction(ActionEvent actionEvent) throws IOException {
+        inputYearButton.setDisable(true);
+        startMonthEndMonthButton.setDisable(true);
+        reportType = ReportType.QUICK_BUTTON_2;
+        confirmButtonOnAction(actionEvent);
+    }
+
+    @FXML
+    private void quickButton3OnAction(ActionEvent actionEvent) throws IOException {
+        inputYearButton.setDisable(true);
+        startMonthEndMonthButton.setDisable(true);
+        reportType = ReportType.QUICK_BUTTON_3;
+        confirmButtonOnAction(actionEvent);
+    }
+
+    @FXML
     private void confirmButtonOnAction(ActionEvent actionEvent) throws IOException {
         if (reportType == ReportType.INPUT_MONTH) {
             Date firstDate = DateHandler.getFirstDayOfThisMonth(startDateTextField.getText());
             Date lastDate = DateHandler.getLastDayOfThisMonth(startDateTextField.getText());
+            List<TransactionObject> filteredTransactions = transactionHandler.getTransactionsFilterByTime(
+                    firstDate, lastDate);
+            ReportList reportList = ReportHandler.getTypeReports(filteredTransactions);
+            confirmButton.getScene().getWindow().setUserData(reportList);
+            StageHandler.goToView(actionEvent, SystemConfiguration.typeReportViewPath);
+        } else if (reportType == ReportType.QUICK_BUTTON_1) {
+            Date firstDate = DateHandler.getFirstDayOfThisMonth(quickButton1.getText());
+            Date lastDate = DateHandler.getLastDayOfThisMonth(quickButton1.getText());
+            List<TransactionObject> filteredTransactions = transactionHandler.getTransactionsFilterByTime(
+                    firstDate, lastDate);
+            ReportList reportList = ReportHandler.getTypeReports(filteredTransactions);
+            confirmButton.getScene().getWindow().setUserData(reportList);
+            StageHandler.goToView(actionEvent, SystemConfiguration.typeReportViewPath);
+        } else if (reportType == ReportType.QUICK_BUTTON_2) {
+            Date firstDate = DateHandler.getFirstDayOfThisMonth(quickButton2.getText());
+            Date lastDate = DateHandler.getLastDayOfThisMonth(quickButton2.getText());
+            List<TransactionObject> filteredTransactions = transactionHandler.getTransactionsFilterByTime(
+                    firstDate, lastDate);
+            ReportList reportList = ReportHandler.getTypeReports(filteredTransactions);
+            confirmButton.getScene().getWindow().setUserData(reportList);
+            StageHandler.goToView(actionEvent, SystemConfiguration.typeReportViewPath);
+        } else if (reportType == ReportType.QUICK_BUTTON_3) {
+            Date firstDate = DateHandler.getFirstDayOfThisMonth(quickButton3.getText());
+            Date lastDate = DateHandler.getLastDayOfThisMonth(quickButton3.getText());
             List<TransactionObject> filteredTransactions = transactionHandler.getTransactionsFilterByTime(
                     firstDate, lastDate);
             ReportList reportList = ReportHandler.getTypeReports(filteredTransactions);
